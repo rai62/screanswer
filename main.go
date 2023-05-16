bef8d1c1aa215f94853a5093a93e4cb3169d2ad6
Raiya <83504221+rai62@users.noreply.github.com>
2023-05-16 12:49:25 +0900

    Release v1.0.7 (#16)

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ var (
 
 	app = &cli.App{
 		Name:    "screanswer",
-		Version: "v1.0.6",
+		Version: "v1.0.7",
 		Usage: "Capture a text on the screen, copy it to the clipboard, " +
 			"answer it automatically using ChatGPT, " +
 			"and read the output aloud in a system voice",
