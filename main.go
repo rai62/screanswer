0f13c2116a1f33106d3faec632ae358668e6de43
rai62 <r6a2i0@gmail.com>
2023-05-15 20:07:19 -0700

    Remove init()

--- main.go
+++ main.go
@@ -8,55 +8,51 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
-var app *cli.App
+var app = &cli.App{
+	Name:    "screanswer",
+	Version: "v1.0.6",
+	Usage: "Capture a text on the screen, copy it to the clipboard, " +
+		"answer it automatically using ChatGPT, " +
+		"and read the output aloud in a system voice",
+	Commands: []*cli.Command{
+		&captureCommand,
+		&answerCommand,
+		&speakCommand,
+	},
+	Flags: []cli.Flag{answerClipFlag},
+	Action: func(ctx *cli.Context) error {
+		answerQueue := make(chan string, 10)
+		defer close(answerQueue)
 
-func init() {
-	app = &cli.App{
-		Name:    "screanswer",
-		Version: "v1.0.6",
-		Usage: "Capture a text on the screen, copy it to the clipboard, " +
-			"answer it automatically using ChatGPT, " +
-			"and read the output aloud in a system voice",
-		Commands: []*cli.Command{
-			&captureCommand,
-			&answerCommand,
-			&speakCommand,
-		},
-		Flags: []cli.Flag{answerClipFlag},
-		Action: func(ctx *cli.Context) error {
-			answerQueue := make(chan string, 10)
-			defer close(answerQueue)
+		speakQueue := make(chan SpeakRequest, 10)
+		defer close(speakQueue)
 
-			speakQueue := make(chan SpeakRequest, 10)
-			defer close(speakQueue)
+		captureClient, err := NewCaptureClient(ctx.Context)
+		if err != nil {
+			return err
+		}
+		defer captureClient.client.Close()
 
-			captureClient, err := NewCaptureClient(ctx.Context)
-			if err != nil {
-				return err
-			}
-			defer captureClient.client.Close()
+		answerClient, err := NewAnswerClient(ctx.Context, ctx.Bool(answerClipFlag.Name))
+		if err != nil {
+			return err
+		}
 
-			answerClient, err := NewAnswerClient(ctx.Context, ctx.Bool(answerClipFlag.Name))
-			if err != nil {
-				return err
-			}
+		speakClient, err := NewSpeakClient(ctx.Context, 48000)
+		if err != nil {
+			return err
+		}
+		defer speakClient.client.Close()
 
-			speakClient, err := NewSpeakClient(ctx.Context, 48000)
-			if err != nil {
-				return err
-			}
-			defer speakClient.client.Close()
+		var wg sync.WaitGroup
+		wg.Add(3)
+		go captureClient.CaptureWithQueue(&wg, answerQueue)
+		go answerClient.AnswerWithQueue(&wg, answerQueue, speakQueue)
+		go speakClient.SpeakWithQueue(&wg, speakQueue)
+		wg.Wait()
 
-			var wg sync.WaitGroup
-			wg.Add(3)
-			go captureClient.CaptureWithQueue(&wg, answerQueue)
-			go answerClient.AnswerWithQueue(&wg, answerQueue, speakQueue)
-			go speakClient.SpeakWithQueue(&wg, speakQueue)
-			wg.Wait()
-
-			return nil
-		},
-	}
+		return nil
+	},
 }
 
 func main() {
